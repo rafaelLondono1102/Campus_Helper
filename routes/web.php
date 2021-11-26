0062626bb465fbafc5c8817ebd106f12205bebbd
hernando pinzon <hernandonunez13@gmail.com>
2021-11-25 22:31:30 -0500

    Se implemento la confirmacion de correo electronico utilizando metodos de laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LandmarkController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +20,11 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    
+    Route::resource('landmarks', LandmarkController::class);
+});
+

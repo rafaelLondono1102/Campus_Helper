134877263499736744001b5f28c4161bc9513a24
hernando pinzon <hernandonunez13@gmail.com>
2021-12-06 14:26:04 -0500

    Se agregaron botones en el navbar

---
+++ resources/views/layouts/navbar_item_forums.blade.php
@@ -0,0 +1,14 @@
+<li class="nav-item dropdown">
+    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+        Foros
+    </a>
+
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="{{ route('forums.index') }}">
+            Listar
+        </a>
+        <a class="dropdown-item" href="{{ route('forums.create') }}">
+            Crear
+        </a>
+    </div>
+ </li>

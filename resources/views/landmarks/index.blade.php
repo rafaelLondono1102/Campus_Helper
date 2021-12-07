ffae9366c30c096f0ccd633d8e5695103699ab2f
hernando pinzon <hernandonunez13@gmail.com>
2021-12-07 12:52:38 -0500

    Se oculto botones eliminar y editar para los usuarios que no son admins

--- resources/views/landmarks/index.blade.php
+++ resources/views/landmarks/index.blade.php
@@ -20,15 +20,20 @@
             <a href=" {{ route("landmarks.show", $landmark->id)}}" title="Visitar a este landmarke"> {{$landmark->name}} </a>
            
             <div class="btn-group" role="group" aria-label="Basic mixed styles example"> 
-                <a type="button" class="btn btn-warning mt-3" href="{{ route('landmarks.edit', $landmark->id)}}">Editar</a>
-              
+                
+              @if (Auth::check())
+                  @if (Auth::user()->type == 'admin')
+                  
+                    <a type="button" class="btn btn-warning mt-3" href="{{ route('landmarks.edit', $landmark->id)}}">Editar</a>
+                    {{ Form::open(['route'=>['landmarks.destroy',$landmark->id],'method'=>'delete',
+                    'onsubmit'=> 'return confirm(\'¿Está seguro de que desea remover el landmarke?\n ¡Esta acción no se puede deshacer!\')']) }}
+                        <button type="submit" class="btn btn-danger mt-3" href="{{ route('landmarks.destroy', $landmark->id)}}">Remover</button>
+                    {!! Form::close() !!}
+                  
+                  @endif
+              @endif
 
-                {{ Form::open(['route'=> 
-                ['landmarks.destroy',$landmark->id],
-                'method'=>'delete',
-                'onsubmit'=> 'return confirm(\'¿Está seguro de que desea remover el landmarke?\n ¡Esta acción no se puede deshacer!\')']) }}
-                  <button type="submit" class="btn btn-danger mt-3" href="{{ route('landmarks.destroy', $landmark->id)}}">Remover</button>
-                {!! Form::close() !!}
+                
             </div>
         </li>  
         @endforeach
--- resources/views/landmarks/show.blade.php
+++ resources/views/landmarks/show.blade.php
@@ -31,14 +31,20 @@
             
             
             <div class="btn-group mt-3" role="group" aria-label="Basic mixed styles example"> 
-                <a type="button" class="btn btn-warning mt-1" href="{{ route('landmarks.edit', $landmark->id)}}">Editar</a>
+                @if (Auth::check())
+                  @if (Auth::user()->type == 'admin')
+
+                    <a type="button" class="btn btn-warning mt-1" href="{{ route('landmarks.edit', $landmark->id)}}">Editar</a>
+                    {{ Form::open(['route'=> 
+                        ['landmarks.destroy',$landmark->id],
+                        'method'=>'delete',
+                        'onsubmit'=> 'return confirm(\'¿Está seguro de que desea remover este punto de interes?\n ¡Esta acción no se puede deshacer!\')']) }}
+                        <button type="submit" class="btn btn-danger mt-1" href="{{ route('landmarks.destroy', $landmark->id)}}">Remover</button>
+                    {!! Form::close() !!}
+                  
+                  @endif
+              @endif
                 
-                {{ Form::open(['route'=> 
-                    ['landmarks.destroy',$landmark->id],
-                    'method'=>'delete',
-                    'onsubmit'=> 'return confirm(\'¿Está seguro de que desea remover este punto de interes?\n ¡Esta acción no se puede deshacer!\')']) }}
-                    <button type="submit" class="btn btn-danger mt-1" href="{{ route('landmarks.destroy', $landmark->id)}}">Remover</button>
-                {!! Form::close() !!}
             </div>
         
         </div>
